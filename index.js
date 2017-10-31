abad4ca5bd4a4de34e5884f229f6c675e5236433
lihe3 <lihe3@corp.beisencorp.com>
2017-10-31 10:57:42 +0800

    build:publicPath add chunkFileName

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ module.exports = function(options) {
         entry: entry,
         output: {
             path: path.resolve(root, "dist"),
-            filename: isProduction
-                ? "[name]-[chunkhash].min.js"
-                : "[name].js",
+            chunkFilename: isProduction
+                ? "[name]-[chunkhash].chunk.min.js"
+                : "[name].chunk.js",
+            filename: isProduction ? "[name]-[chunkhash].min.js" : "[name].js",
             library: library,
             libraryTarget: "umd",
-            publicPath: asset_path || "./"
+            publicPath: asset_path || ""
         },
         module: {
             rules: require("./rules/index.js")
