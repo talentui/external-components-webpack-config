cac9be8230673e9abe6397110788d2fab4ad5e27
lihe3 <lihe3@corp.beisencorp.com>
2017-11-17 18:04:45 +0800

    build:调整css文件夹结构  定义新的常量

--- constants.js
+++ constants.js
@@ -1,9 +1,11 @@
 const { NODE_ENV, npm_package_name } = process.env;
 const production = "production"
 const library = npm_package_name.replace(/@|\-|\//g,'_');//以package name作为umd全局的变量名
+const rawName = npm_package_name;//项目名称
 
 const isProduction = NODE_ENV === production;
 module.exports = {
     isProduction,
-    library
+    library,
+    rawName
 };
--- rules/extract-scss-rule.js
+++ rules/extract-scss-rule.js
@@ -14,6 +14,6 @@ module.exports = {
             },
             "sass-loader"
         ],
-        publicPath:'../'    
+        publicPath:'./'    
     }),
 };
