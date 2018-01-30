237cbb7f5b7ed65ee6aa5a6b3403d5a574b329de
lihe3 <lihe3@corp.beisencorp.com>
2018-01-30 09:44:56 +0800

    fix alias

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { isProduction, library } = require("./constants");
 const { asset_path, npm_package_version } = process.env;
 module.exports = function(options) {
-    const { entry, root, dllList, alias } = options;
+    const { entry, root = process.pwd(), dllList, alias } = options;
     const plugins = require("./plugins")(options);
     return {
         entry: entry,
@@ -24,12 +24,7 @@ module.exports = function(options) {
         plugins: plugins,
         resolve: {
             extensions: [".tsx", ".ts", ".js", ".jsx", ".vue"],
-            alias: Object.assign(
-                {
-                    "&": options.moduleScope
-                },
-                alias
-            )
+            alias: alias
         }
     };
 };
