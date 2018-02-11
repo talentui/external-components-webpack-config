5bc0c2218736c7859a834211c6fd33ffc74c9609
lihe3 <lihe3@corp.beisencorp.com>
2018-02-11 14:42:58 +0800

    1.1.0

--- bin/build-prod.sh
+++ bin/build-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf dist # clear
-NODE_ENV=production webpack --config
+
+NODE_ENV=production webpack --config \
 ./node_modules/@talentui/external-components-webpack-config/webpack.config.js # build-prod
--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf dist # clear
-check=on webpack --config
-./node_modules/@talentui/external-components-webpack-config/webpack.config.js #执行构建
+
+check=on webpack --config ./node_modules/@talentui/external-components-webpack-config/webpack.config.js #执行构建
