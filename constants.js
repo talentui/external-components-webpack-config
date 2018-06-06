28affdab354c0d2b4810664fc0ffd0891851fa5d
lihe3 <lihe3@corp.beisencorp.com>
2018-06-06 09:44:30 +0800

    fix bug

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const { NODE_ENV, npm_package_name, npm_package_appId } = process.env;
+const { NODE_ENV, npm_package_name, npm_package_appId, npm_package_componentCode } = process.env;
 const production = "production"
 const library = npm_package_name.replace(/@|\-|\//g,'_');//以package name作为umd全局的变量名
 const packageName = npm_package_name;//项目名称
