8c59e862049db06dd198a8af4fd5f0ebce32c96a
lihe3 <lihe3@corp.beisencorp.com>
2018-06-06 09:41:13 +0800

    build:component register

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import components from "&/index.js";// 外部组件
+import component from "&/index.js";// 外部组件
 import componentRegistry from "@talentui/external-component-registry";//组件注册工具，已注入到dll-react
 /**
  * webpack 按需加载，设置public path
@@ -7,4 +7,4 @@ __webpack_public_path__ = "//stnew03.beisen.com/ux/upaas/" + process.env.package
 /**
  * 组件注册
 */
-componentRegistry.set(process.env.packageName, components);
+componentRegistry.set(process.env.componentCode, component);
