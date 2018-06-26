572901a8c4ffaa619554d9ef49f7cb2f2e44f683
lihe3 <lihe3@corp.beisencorp.com>
2018-06-26 18:45:15 +0800

    fix bug: webpack public path at top

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import component from "&/index.js";// 外部组件
-import componentRegistry from "@talentui/external-component-registry";//组件注册工具，已注入到dll-react
 /**
  * webpack 按需加载，设置public path
 */
 __webpack_public_path__ = "//stnew03.beisen.com/ux/upaas/" + process.env.packageName + "/release/dist/";
+
+import component from "&/index.js";// 外部组件
+import componentRegistry from "@talentui/external-component-registry";//组件注册工具，已注入到dll-react
 /**
  * 组件注册
 */
