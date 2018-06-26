701f6b773bb8d4ddffe801171208a911c9a05fb7
lihe3 <lihe3@corp.beisencorp.com>
2018-06-26 11:30:32 +0800

    fix bug : read dllList from package.json

--- constants.js
+++ constants.js
@@ -2,17 +2,17 @@ const {
     NODE_ENV,
     npm_package_name,
     npm_package_appId,
-    npm_package_componentCode,
-    npm_package_dllList
+    npm_package_componentCode
 } = process.env;
+const path = require('path');
 const production = "production";
 const library = npm_package_name.replace(/@|\-|\//g, "_"); //以package name作为umd全局的变量名
 const packageName = npm_package_name; //项目名称
 const appId = npm_package_appId;
 const componentCode = npm_package_componentCode; //组件编码
-const dllList = npm_package_dllList;
 const isProduction = NODE_ENV === production;
 const root = process.cwd();
+const dllList = require(path.resolve(root,'package.json')).dllList;
 module.exports = {
     isProduction,
     library,
