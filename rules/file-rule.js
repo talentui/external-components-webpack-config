179d9cdbfcfb7596d8a4652a1b83c6ba4b20a3cc
lihe3 <lihe3@corp.beisencorp.com>
2018-09-11 11:56:19 +0800

    build:在file loader添加hash

--- rules/file-rule.js
+++ rules/file-rule.js
@@ -1,8 +1,11 @@
+const { isProduction } = require("../constants");
 module.exports = {
     test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[^('|")]*)?$/,
     loader: "file-loader",
     options: {
-        name: "images/[name].[ext]",
+        name: isProduction
+            ? "images/[name]-[hash].[ext]"
+            : "images/[name].[ext]",
         useRelativePath: true
     }
 };
