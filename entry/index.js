82a062d86bd8e3ed3d2362577d7ae944e42b27c8
lihe3 <lihe3@corp.beisencorp.com>
2018-11-16 12:54:42 +0800

    fix(entry): 兼容默认是组件的情况

--- entry/index.js
+++ entry/index.js
@@ -1,4 +1,3 @@
-
 /**
  * 构建入口
  * webpack 按需加载，设置public path
@@ -7,16 +6,19 @@ __webpack_public_path__ =
     "//stnew03.beisen.com/ux/upaas/" +
     process.env.packageName +
     "/release/dist/";
-import propsLoader from './propsLoader';
-import rawComponent from '_/src/index.js'
+import propsLoader from "./propsLoader";
+import rawComponent from "_/src/index.js";
 const component = propsLoader(rawComponent);
 /**
  * 组件注册
  */
-const componentCode = process.env.componentCode;//组件 | 布局的编码
-const appId = process.env.appId;//组件的应用ID
-//如果是组件的话
-if(process.env.projectType === 'component'){
+const componentCode = process.env.componentCode; //组件 | 布局的编码
+const appId = process.env.appId; //组件的应用ID
+if (process.env.projectType === "layout") {
+    //如果是布局的话
+    window._talentui_registry.set("_layout", component);
+} else {
+    //如果是组件的话
     window._talentui_registry.update("_externalComp", function(externalComp) {
         if (externalComp === undefined) {
             const newValue = {};
@@ -27,11 +29,12 @@ if(process.env.projectType === 'component'){
             const curValue = externalComp[process.env.appId] || {};
             const newObj = {};
             newObj[componentCode] = component;
-            externalComp[process.env.appId] = Object.assign({}, curValue, newObj);
+            externalComp[process.env.appId] = Object.assign(
+                {},
+                curValue,
+                newObj
+            );
             return externalComp;
         }
     });
-}else{
-    //如果是布局的话
-    window._talentui_registry.set("_layout", component)
 }
