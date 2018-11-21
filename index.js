f5209bebca816b502f21e429bc9dd46daea6a212
lihe3 <lihe3@corp.beisencorp.com>
2018-11-21 10:59:15 +0800

    fix(index.js): webpack publicPath，解决图片资源的路径问题

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const path = require("path");
 const merge = require("webpack-merge");
 const Analyzer = require("webpack-bundle-analyzer");
 const root = fs.realpathSync(process.cwd());
-const { appId, componentCode, name, version, projectType } = require(path.resolve(
-    root,
-    "package.json"
-));
+const {
+    appId,
+    componentCode,
+    name,
+    version,
+    projectType
+} = require(path.resolve(root, "package.json"));
 const library = name.replace(/@|\-|\//g, "_");
 const buildProd =
     require("@talentui/webpack-config/src/helpers/parse-mode")() ===
@@ -14,11 +17,12 @@ const buildProd =
 
 const webpackConfig = require("@talentui/webpack-config")({
     entry: {
-        main: path.resolve(__dirname,"lib/index.js")
+        main: path.resolve(__dirname, "lib/index.js")
     },
     port: 3001,
-    language:"mixed",
+    language: "mixed",
     extractStyles: false,
+    publichPath: "//stnew03.beisen.com/ux/upaas/" + name + "/release/dist/",
     define: {
         "process.env": {
             library: JSON.stringify(library),
